bca902245beadb3571e5b0d4a45112d67f043a88
Jesse Ward-Bond <jessewb.c@gmail.com>
2024-04-15 16:40:38 -0400

    Formatting

--- satseg/features_extract.py
+++ satseg/features_extract.py
@@ -13,7 +13,7 @@ def deep_features(
     """Extract descriptors from pretrained DINO model; Create an adj matrix from descriptors
 
     Args:
-        image_tensor (torch.Tensor): Tensor of size
+        image_tensor (torch.Tensor): Tensor of size BxCxHxW
         extractor (DINO): Initialized model to extract descriptors from
         layer (str): Layer to extract the descriptors from
         facet (str): Facet to extract the descriptors from (key, value, query)
@@ -22,11 +22,11 @@ def deep_features(
         device (str, optional): Training device. Defaults to 'cuda'.
 
     Returns:
-        np.ndarray: (NxB)xD
+        np.ndarray: (N*B)xD, usually just NxD
     """
 
     # images to deep_features.
-    # input is a tensor of size 1xBxHxW
+    # input is size: BxCxHxW
     # output is size: Bx1xNxD
     deep_features = (
         extractor.extract_descriptors(
@@ -36,13 +36,14 @@ def deep_features(
         .numpy()
     )
 
-    # batch X height/patchsize * width/patchsize X deep_features size
+    # in: Bx1xNxD
+    # out: BxNxD
     deep_features = np.squeeze(deep_features, axis=1)
 
-    # batch * height/patchsize * width/patchsize X deep_features size
+    # in: BxNxD
+    # out: (B*N)xD
     deep_features = deep_features.reshape(
         (deep_features.shape[0] * deep_features.shape[1], deep_features.shape[2])
     )
 
-    # deep_features size X batch * (height* width/(patchsize ^2))
     return deep_features
